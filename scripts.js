78b1a322274656de1dce659f3971bdc59c549f38
JoJaCode <46225636+JoJaCode@users.noreply.github.com>
2024-08-08 23:07:28 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,7 +12,6 @@ document.addEventListener("DOMContentLoaded", function() {
             applyTranslations(data[lang]);
         });
 
-    // Використовуємо делегування подій для кнопок мови
     document.body.addEventListener('click', function(event) {
         if (event.target.classList.contains('lang-button') || event.target.parentElement.classList.contains('lang-button')) {
             const button = event.target.closest('.lang-button');
@@ -27,15 +26,26 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector('.pubg-text').textContent = translations.pubg;
         document.querySelector('.overwatch-text').textContent = translations.overwatch;
 
+        // Створення списку завдань для PUBG
+        const pubgTasks = translations.pubg_tasks;
+        let pubgTasksHtml = '';
+        for (let i = 1; i <= Object.keys(pubgTasks).length; i++) {
+            pubgTasksHtml += `<li>${pubgTasks[`pubg_task${i}`]}</li>`;
+        }
+
+        // Створення списку завдань для Overwatch 2
+        const overwatchTasks = translations.overwatch_tasks;
+        let overwatchTasksHtml = '';
+        for (let i = 1; i <= Object.keys(overwatchTasks).length; i++) {
+            overwatchTasksHtml += `<li>${overwatchTasks[`overwatch_task${i}`]}</li>`;
+        }
+
         // Зміна контенту для PUBG
         const pubgContent = document.querySelector('.pubg-content .challenge-overlay');
         pubgContent.innerHTML = `
             <label for="toggle-home" class="back-button">${translations.back}</label>
             <h2>${translations.pubg_challenges}</h2>
-            <ul>
-                <li>${translations.pubg_task1}</li>
-                <li>${translations.pubg_task2}</li>
-            </ul>
+            <ul>${pubgTasksHtml}</ul>
             <div class="buttons">
                 <a href="https://www.twitch.tv/" class="button">${translations.twitch}</a>
                 <a href="https://discord.com/" class="button">${translations.discord}</a>
@@ -51,10 +61,7 @@ document.addEventListener("DOMContentLoaded", function() {
         overwatchContent.innerHTML = `
             <label for="toggle-home" class="back-button">${translations.back}</label>
             <h2>${translations.overwatch_challenges}</h2>
-            <ul>
-                <li>${translations.overwatch_task1}</li>
-                <li>${translations.overwatch_task2}</li>
-            </ul>
+            <ul>${overwatchTasksHtml}</ul>
             <div class="buttons">
                 <a href="https://www.twitch.tv/" class="button">${translations.twitch}</a>
                 <a href="https://discord.com/" class="button">${translations.discord}</a>
